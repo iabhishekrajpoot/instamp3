14d9f34fd5c21cbe010f49f4679cf26381cd7d6e
iabhishekrajpoot <84550714+iabhishekrajpoot@users.noreply.github.com>
2021-12-02 19:15:27 +0530

    Update index.js

--- index.js
+++ index.js
@@ -3,46 +3,26 @@ const express = require('express')();
 var ffmpeg = require('fluent-ffmpeg');
 const app = express;
 
-/**
-
- *    input - string, path of input file
-
- *    output - string, path of output file
-
- *    callback - function, node-style callback fn (error, result)        
-
- */
-
-app.get('/', (req, res)=>{
-url = decodeURIComponent(req.query.url);
-res.send("running");
-console.log(url);
+app.get('/', (re, res) => {
+ url = decodeURIComponent(req.query.url);
+ res.send("running");
+ console.log(url);
 });
 
 app.get("/api", async (req, res) => {
-res.attachment('output.mp3');
+  res.attachment('output.mp3');
   res.contentType('audio/mp3');
   url = decodeURIComponent(req.query.url);
 
 ffmpeg(url)
-
-        .toFormat('mp3')
-
-        .on('end', function() {                    
-
-            console.log('conversion ended');
-
-        
-
+ .toFormat('mp3')
+ .on('end', function() {                    
+        console.log('conversion ended');
         })
-.on('error', function(error){
-
-           console.log('error: ' + error.message);
-
-          
-})
-
-        .pipe(res,{end:true})
+ .on('error', function(error){
+        console.log('error: ' + error.message);
+        })
+ .pipe(res,{end:true})
 });
 
 
